79c81fbe53ff7539e3489f723e94d9e67d3c1dec
Zapata <rzapata@gmail.com>
2025-12-11 22:23:34 -0500

    Fixed Rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "second/index"
   get "dashboard" => "pages#dashboard", as: :dashboard
   get "contact" => "pages#contact", as: :contact
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
