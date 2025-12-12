5d9c8141773a6522d801c09f7f718828ef300114
Zapata <rzapata@gmail.com>
2025-12-11 22:21:40 -0500

    Added controllers and views to test authentication

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  allow_unauthenticated_access only: :home
+    
+  def home
+    redirect_to dashboard_path if authenticated?
+  end
+  
+  def dashboard
+     redirect_to new_session_path unless authenticated?
+  end
+
+  def contact
+    render 'contact'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   get "second/index"
-  get "sample/index"
+  get "dashboard" => "pages#dashboard", as: :dashboard
+  get "contact" => "pages#contact", as: :contact
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,5 +16,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "sample#index"
+  root "pages#home"
 end
